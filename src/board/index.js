c277e830ad3a26e6696e95755131a7ff39c306d1
lrz <lrz_china@163.com>
2020-08-18 13:43:15 +0800

    immutable data,function component, storybook

--- src/board/index.js
+++ src/board/index.js
@@ -6,6 +6,7 @@ export class Board extends React.Component {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
 
@@ -17,12 +18,30 @@ export class Board extends React.Component {
   
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      /**
+       * 不可变性在 React 中非常重要
+       * 
+       * 不直接修改（或改变底层数据）这种方式有以下几点好处：
+       * 
+       * 简化复杂的功能
+       *   撤销和恢复功能在开发中是一个很常见的需求。不直接在数据上修改可以让我们追溯并复用游戏的历史记录。
+       * 
+       * 跟踪数据的改变
+       *  如果直接修改数据，那么就很难跟踪到数据的改变。跟踪数据的改变需要可变对象可以与改变之前的版本进行对比，这样整个对象树都需要被遍历一次。
+       *  跟踪不可变数据的变化相对来说就容易多了。如果发现对象变成了一个新对象，那么我们就可以说对象发生改变了。
+       * 
+       * 确定在 React 中何时重新渲染
+       *  不可变性最主要的优势在于它可以帮助我们在 React 中创建 pure components。我们可以很轻松的确定不可变数据是否发生了改变，从而确定何时对组件进行重新渲染
+       */
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
--- src/square/index.js
+++ src/square/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
-export class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        console.warn(props)
-    }
-    render() {
-        return (
-        <button 
-            className="square"  
-            onClick={() => this.props.onClick()}
-        >
-            {this.props.value}
+// export class Square extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         console.warn(props)
+//     }
+//     render() {
+//         return (
+//         <button 
+//             className="square"  
+//             onClick={() => this.props.onClick()}
+//         >
+//             {this.props.value}
+//         </button>
+//         );
+//     }
+// }
+
+/**
+ * 函数组件
+ * 如果你想写的组件只包含一个 render 方法，并且不包含 state，那么使用函数组件就会更简单。我们不需要定义一个继承于 React.Component 的类，我们可以定义一个函数
+ */
+
+export function Square (props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+        {props.value}
         </button>
-        );
-    }
+    );
 }
