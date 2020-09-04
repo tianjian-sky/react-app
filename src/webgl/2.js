8ade31a54310c2251513e2360456b66c22580e3f
tianjian_yu <532861047@qq.com>
2020-09-04 21:42:33 +0800

    变换矩阵

--- src/webgl/2.js
+++ src/webgl/2.js
@@ -3,6 +3,7 @@ import _2_1 from './2/1'
 import _2_2 from './2/2'
 import _2_3 from './2/3'
 import _2_4 from './2/4'
+import _2_5 from './2/5'
 
 export default class extends React.Component {
     constructor(props) {
@@ -16,6 +17,7 @@ export default class extends React.Component {
                     <_2_2/>
                     <_2_3/>
                     <_2_4/>
+                    <_2_5/>
                 </div>
             </div>
         );
