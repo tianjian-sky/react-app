cf792ca7a3804330af5c37eef965762ebe710e8e
tianjian_yu <532861047@qq.com>
2020-09-13 13:41:06 +0800

    animation

--- src/webgl/3.js
+++ src/webgl/3.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import _3_1 from './3/1' 
-
+import _3_2 from './3/2' 
 
 export default class extends React.Component {
     constructor(props) {
@@ -11,6 +11,7 @@ export default class extends React.Component {
             <div className="ch3">
                 <div className="webgl contaner">
                     <_3_1/>
+                    <_3_2/>
                 </div>
             </div>
         );
