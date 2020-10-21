abcf10e49443653ca505554bdfb87e1654e36286
tianjian_yu <532861047@qq.com>
2020-10-21 21:22:54 +0800

    逐个片元光照

--- src/webgl/6.js
+++ src/webgl/6.js
@@ -4,6 +4,7 @@ import _6_1 from './6/1'
 import _6_2 from './6/2' 
 import _6_3 from './6/3'
 import _6_4 from './6/4'
+import _6_5 from './6/5'
 
 export default class extends React.Component {
     constructor(props) {
@@ -18,6 +19,7 @@ export default class extends React.Component {
                     <_6_2/>
                     <_6_3/>
                     <_6_4/>
+                    <_6_5/>
                 </div>
             </div>
         );
