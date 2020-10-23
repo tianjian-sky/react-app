42508c72255e91704be258548d6b26022b9f8eed
tianjian_yu <532861047@qq.com>
2020-10-23 21:40:48 +0800

    多关节1

--- src/webgl/7.js
+++ src/webgl/7.js
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import _7_1 from './7/1' 
+import _7_2 from './7/2' 
 
 export default class extends React.Component {
     constructor(props) {
@@ -10,6 +11,7 @@ export default class extends React.Component {
             <div className="ch3">
                 <div className="webgl contaner">
                     <_7_1/>
+                    <_7_2/>
                 </div>
             </div>
         );
