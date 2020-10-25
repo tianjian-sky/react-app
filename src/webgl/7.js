ce0436559f0baa9a8c6e22eb8849365f52e25a2a
tianjian_yu <532861047@qq.com>
2020-10-25 14:17:21 +0800

    立方体旋转

--- src/webgl/7.js
+++ src/webgl/7.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import _7_1 from './7/1' 
 import _7_2 from './7/2' 
+import _7_3 from './7/3' 
 
 export default class extends React.Component {
     constructor(props) {
@@ -12,6 +13,7 @@ export default class extends React.Component {
                 <div className="webgl contaner">
                     <_7_1/>
                     <_7_2/>
+                    <_7_3/>
                 </div>
             </div>
         );
