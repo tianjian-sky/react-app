6c0c8e9dca601ef44793cf2cd9b2965ff9ad8ead
tianjian-sky <532861047@qq.com>
2021-10-16 15:48:05 +0800

    instance draw init

--- src/webgl/8.js
+++ src/webgl/8.js
@@ -1,5 +1,6 @@
-import React from 'react'; 
-import _8_1 from './8/1' 
+import React from 'react';
+import _8_1 from './8/1'
+import _8_2 from './8/2'
 
 export default class extends React.Component {
     constructor(props) {
@@ -9,7 +10,8 @@ export default class extends React.Component {
         return (
             <div className="ch3">
                 <div className="webgl contaner">
-                    <_8_1/>
+                    <_8_1 />
+                    <_8_2 />
                 </div>
             </div>
         );
