e47e691a028ec571af466c5727267e49890566cd
tianjian-sky <532861047@qq.com>
2022-02-11 17:40:08 +0800

    一次渲染多个着色器调用example

--- src/webgl/3.js
+++ src/webgl/3.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import _3_1 from './3/1' 
-import _3_2 from './3/2' 
+import _3_1 from './3/1'
+import _3_2 from './3/2'
+import _3_3 from './3/3'
 
 export default class extends React.Component {
     constructor(props) {
@@ -10,8 +11,9 @@ export default class extends React.Component {
         return (
             <div className="ch3">
                 <div className="webgl contaner">
-                    <_3_1/>
-                    <_3_2/>
+                    <_3_1 />
+                    <_3_2 />
+                    <_3_3 />
                 </div>
             </div>
         );
